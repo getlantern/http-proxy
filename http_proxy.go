3c04bd9e1093c4ebdcb971a5902fef967511da66
Ulysses Aalto <uaalto@getlantern.org>
2015-11-09 13:07:14 -0500

    Private proxy

---
+++ http_proxy.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/getlantern/golog"
+	"github.com/getlantern/measured"
+
+	"github.com/getlantern/http-proxy/commonfilter"
+	"github.com/getlantern/http-proxy/forward"
+	"github.com/getlantern/http-proxy/httpconnect"
+	"github.com/getlantern/http-proxy/logging"
+	"github.com/getlantern/http-proxy/server"
+
+	"github.com/getlantern/http-proxy-lantern/report"
+	"github.com/getlantern/http-proxy-lantern/tokenfilter"
+)
+
+var (
+	testingLocal = false
+	log          = golog.LoggerFor("main")
+
+	help          = flag.Bool("help", false, "Get usage help")
+	keyfile       = flag.String("key", "", "Private key file name")
+	certfile      = flag.String("cert", "", "Certificate file name")
+	https         = flag.Bool("https", false, "Use TLS for client to proxy communication")
+	addr          = flag.String("addr", ":8080", "Address to listen")
+	maxConns      = flag.Uint64("maxconns", 0, "Max number of simultaneous connections allowed connections")
+	idleClose     = flag.Uint64("idleclose", 30, "Time in seconds that an idle connection will be allowed before closing it")
+	token         = flag.String("token", "", "Lantern token")
+	enableFilters = flag.Bool("enablefilters", false, "Enable Lantern-specific filters")
+	enableReports = flag.Bool("enablereports", false, "Enable stats reporting")
+	logglyToken   = flag.String("logglytoken", "", "Token used to report to loggly.com, not reporting if empty")
+)
+
+func main() {
+	var err error
+
+	_ = flag.CommandLine.Parse(os.Args[1:])
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	// TODO: use real parameters
+	err = logging.Init("instanceid", "version", "releasedate", *logglyToken)
+	if err != nil {
+		log.Error(err)
+	}
+
+	forwarder, err := forward.New(nil, forward.IdleTimeoutSetter(time.Duration(*idleClose)*time.Second))
+	if err != nil {
+		log.Error(err)
+	}
+
+	httpConnect, err := httpconnect.New(forwarder, httpconnect.IdleTimeoutSetter(time.Duration(*idleClose)*time.Second))
+	if err != nil {
+		log.Error(err)
+	}
+
+	tokenFilter, err := tokenfilter.New(httpConnect, tokenfilter.TokenSetter(*token))
+	if err != nil {
+		log.Error(err)
+	}
+
+	commonHandler, err := commonfilter.New(tokenFilter, testingLocal)
+	if err != nil {
+		log.Error(err)
+	}
+
+	srv := server.NewServer(commonHandler)
+
+	if *enableReports {
+		redisAddr := os.Getenv("REDIS_PRODUCTION_URL")
+		if redisAddr == "" {
+			redisAddr = "127.0.0.1:6379"
+		}
+		rp, err := report.NewRedisReporter(redisAddr)
+		if err != nil {
+			log.Errorf("Error connecting to redis: %v", err)
+		} else {
+			measured.Start(20*time.Second, rp)
+			defer measured.Stop()
+		}
+	}
+
+	if *https {
+		err = srv.ServeHTTPS(*addr, *keyfile, *certfile, nil)
+	} else {
+		err = srv.ServeHTTP(*addr, nil)
+	}
+	if err != nil {
+		log.Errorf("Error serving: %v", err)
+	}
+}
