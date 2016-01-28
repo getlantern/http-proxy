7acd93a864ff2a28d7a7f4f155c8e3852eccf096
Ox Cart <ox.to.a.cart@gmail.com>
2016-01-28 13:21:21 -0600

    getlantern/lantern#3451 Doing reverse lookups of ip addresses and only sampling site views

--- http_proxy.go
+++ http_proxy.go
@@ -30,18 +30,19 @@ var (
 	testingLocal = false
 	log          = golog.LoggerFor("lantern-proxy")
 
-	help                   = flag.Bool("help", false, "Get usage help")
-	keyfile                = flag.String("key", "", "Private key file name")
-	certfile               = flag.String("cert", "", "Certificate file name")
-	https                  = flag.Bool("https", false, "Use TLS for client to proxy communication")
-	addr                   = flag.String("addr", ":8080", "Address to listen")
-	maxConns               = flag.Uint64("maxconns", 0, "Max number of simultaneous connections allowed connections")
-	idleClose              = flag.Uint64("idleclose", 30, "Time in seconds that an idle connection will be allowed before closing it")
-	token                  = flag.String("token", "", "Lantern token")
-	enableReports          = flag.Bool("enablereports", false, "Enable stats reporting")
-	logglyToken            = flag.String("logglytoken", "", "Token used to report to loggly.com, not reporting if empty")
-	pprofAddr              = flag.String("pprofaddr", "", "pprof address to listen on, not activate pprof if empty")
-	proxiedSitesTrackingId = flag.String("proxied-sites-tracking-id", "UA-21815217-15", "The Google Analytics property id for tracking proxied sites")
+	help                         = flag.Bool("help", false, "Get usage help")
+	keyfile                      = flag.String("key", "", "Private key file name")
+	certfile                     = flag.String("cert", "", "Certificate file name")
+	https                        = flag.Bool("https", false, "Use TLS for client to proxy communication")
+	addr                         = flag.String("addr", ":8080", "Address to listen")
+	maxConns                     = flag.Uint64("maxconns", 0, "Max number of simultaneous connections allowed connections")
+	idleClose                    = flag.Uint64("idleclose", 30, "Time in seconds that an idle connection will be allowed before closing it")
+	token                        = flag.String("token", "", "Lantern token")
+	enableReports                = flag.Bool("enablereports", false, "Enable stats reporting")
+	logglyToken                  = flag.String("logglytoken", "", "Token used to report to loggly.com, not reporting if empty")
+	pprofAddr                    = flag.String("pprofaddr", "", "pprof address to listen on, not activate pprof if empty")
+	proxiedSitesTrackingId       = flag.String("proxied-sites-tracking-id", "UA-21815217-15", "The Google Analytics property id for tracking proxied sites")
+	proxiedSitesSamplePercentage = flag.Float64("proxied-sites-sample-percentage", 0.01, "The percentage of requests to sample (0.01 = 1%)")
 )
 
 func main() {
@@ -105,7 +106,7 @@ func main() {
 
 	deviceFilterPost := devicefilter.NewPost(commonFilter)
 
-	analyticsFilter := analytics.New(*proxiedSitesTrackingId, deviceFilterPost)
+	analyticsFilter := analytics.New(*proxiedSitesTrackingId, *proxiedSitesSamplePercentage, deviceFilterPost)
 
 	deviceFilterPre, err := devicefilter.NewPre(analyticsFilter)
 	if err != nil {
