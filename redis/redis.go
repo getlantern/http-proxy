2ecd08f8b96aae8585eda0e007553197c96c48f3
Ulysses Aalto <uaalto@getlantern.org>
2016-04-22 13:33:52 -0400

    Added password authentication to Redis client config and flags

--- redis/redis.go
+++ redis/redis.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"fmt"
+	"net/url"
 
 	"gopkg.in/redis.v3"
 
@@ -13,18 +14,32 @@ var (
 	rcs = make(map[string]*redis.Client)
 )
 
-func getClient(redisAddr string) (*redis.Client, error) {
-	if rc, ok := rcs[redisAddr]; ok {
+func getClient(redisUrl string) (*redis.Client, error) {
+	u, err := url.Parse(redisUrl)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse Redis address: %s", err)
+	}
+
+	if u.Host == "" {
+		return nil, fmt.Errorf("Please provide a Redis URL of the form 'redis://[user:pass@host:port]'")
+	}
+
+	if rc, ok := rcs[u.Host]; ok {
 		return rc, nil
 	} else {
+		redisPass := ""
+		if u.User != nil {
+			redisPass, _ = u.User.Password()
+		}
 		rc := redis.NewClient(&redis.Options{
-			Addr: redisAddr,
+			Addr:     u.Host,
+			Password: redisPass,
 		})
 		_, err := rc.Ping().Result()
 		if err != nil {
 			return nil, fmt.Errorf("Unable to ping redis server: %s", err)
 		}
-		rcs[redisAddr] = rc
+		rcs[u.Host] = rc
 		return rc, nil
 	}
 }
