9b732c264f31cd970092f297917a6ec89012a74e
Ulysses Aalto <uaalto@getlantern.org>
2016-05-19 00:06:27 +0200

    Squashed 'bitrate/' changes from 4a56fb4..ce20da1
    ce20da1 Even distribution of limits
    
    git-subtree-dir: bitrate
    git-subtree-split: ce20da15e04f8acc6acd91ab50c792aedd3f4b44

--- controller.go
+++ controller.go
@@ -5,47 +5,50 @@ package bitrate
 
 import (
 	"errors"
+	"math"
 	"sync"
+	"time"
+
+	"github.com/mxk/go-flowrate/flowrate"
 )
 
 var (
 	errorGroupNotExist = errors.New("Group does not exist")
 )
 
-// TODO
-type Status int
-
-type Limiter interface {
-	Done() int64
-	Status() Status
-	SetTransferSize(bytes int64)
-	SetLimit(new int64) (old int64)
-	SetBlocking(new bool) (old bool)
-}
-
 type SharedFlowControllerOptions struct {
-	globalLimit int64
+	GlobalLimit int64
+	Interval    time.Duration
 }
 
 type GroupsMap map[string]*FlowGroup
 
 type SharedFlowController struct {
 	options *SharedFlowControllerOptions
+	ticker  *time.Ticker
 	gMtx    sync.RWMutex
 	groups  GroupsMap
 }
 
 func NewSharedFlowController(opts *SharedFlowControllerOptions) *SharedFlowController {
+	if opts.GlobalLimit == 0 {
+		opts.GlobalLimit = math.MaxInt64
+	}
+	if opts.Interval == 0 {
+		opts.Interval = time.Second
+	}
+
 	s := &SharedFlowController{
 		options: opts,
 		groups:  make(GroupsMap),
+		ticker:  time.NewTicker(opts.Interval),
 	}
-	go s.updateProc()
+	go s.updateFlowGroups()
 
 	return s
 }
 
-func (m *SharedFlowController) AddToGroup(group string, l *Limiter) (isNew bool) {
+func (m *SharedFlowController) AddToGroup(group string, l *flowrate.Limiter) (isNew bool) {
 	m.gMtx.Lock()
 	defer m.gMtx.Unlock()
 	if s, ok := m.groups[group]; ok {
@@ -61,7 +64,7 @@ func (m *SharedFlowController) AddToGroup(group string, l *Limiter) (isNew bool)
 	return
 }
 
-func (m *SharedFlowController) RemoveFromGroup(flowgroup string, l *Limiter) error {
+func (m *SharedFlowController) RemoveFromGroup(flowgroup string, l *flowrate.Limiter) error {
 	m.gMtx.Lock()
 	defer m.gMtx.Unlock()
 	fg, ok := m.groups[flowgroup]
@@ -75,11 +78,13 @@ func (m *SharedFlowController) RemoveFromGroup(flowgroup string, l *Limiter) err
 	return nil
 }
 
-func (m *SharedFlowController) updateProc() {
+func (m *SharedFlowController) updateFlowGroups() {
 	m.gMtx.Lock()
 	defer m.gMtx.Unlock()
 
-	for _, fg := range m.groups {
-		fg.UpdateLimits()
+	for range m.ticker.C {
+		for _, fg := range m.groups {
+			fg.UpdateLimits()
+		}
 	}
 }
