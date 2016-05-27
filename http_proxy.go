3182eb42855deae326125d2c9728d92ac3d954d0
Ox Cart <ox.to.a.cart@gmail.com>
2016-05-27 14:56:16 -0500

    Moved borda stuff into its own package

--- http_proxy.go
+++ http_proxy.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/binary"
 	"flag"
 	"net"
 	"net/http"
@@ -12,7 +10,6 @@ import (
 
 	"github.com/vharitonsky/iniflags"
 
-	borda "github.com/getlantern/borda/client"
 	"github.com/getlantern/golog"
 	"github.com/getlantern/measured"
 	"github.com/getlantern/ops"
@@ -26,6 +23,7 @@ import (
 
 	"github.com/getlantern/http-proxy-lantern/analytics"
 	"github.com/getlantern/http-proxy-lantern/blacklist"
+	"github.com/getlantern/http-proxy-lantern/borda"
 	"github.com/getlantern/http-proxy-lantern/configserverfilter"
 	"github.com/getlantern/http-proxy-lantern/devicefilter"
 	lanternlisteners "github.com/getlantern/http-proxy-lantern/listeners"
@@ -115,49 +113,7 @@ func main() {
 
 	// Configure borda
 	if *bordaReportInterval > 0 {
-		bordaClient := borda.NewClient(&borda.Options{
-			BatchInterval: *bordaReportInterval,
-		})
-		reportToBorda := bordaClient.ReducingSubmitter("proxy_results", 10000, func(existingValues map[string]float64, newValues map[string]float64) {
-			for key, value := range newValues {
-				existingValues[key] += value
-			}
-		})
-		ops.RegisterReporter(func(failure error, ctx map[string]interface{}) {
-			// Sample a subset of device ids
-			deviceID := ctx["device_id"]
-			if deviceID == nil {
-				log.Debugf("No device id, not reporting measurement")
-				return
-			}
-			deviceIDBytes, base64Err := base64.StdEncoding.DecodeString(deviceID.(string))
-			if base64Err != nil {
-				log.Debugf("Error decoding base64 deviceID: %v", base64Err)
-				return
-			}
-			var deviceIDInt uint64
-			if len(deviceIDBytes) < 4 {
-				log.Debugf("DeviceID too small: %v", base64Err)
-			} else if len(deviceIDBytes) < 8 {
-				deviceIDInt = uint64(binary.BigEndian.Uint32(deviceIDBytes))
-			} else {
-				deviceIDInt = binary.BigEndian.Uint64(deviceIDBytes)
-			}
-			if deviceIDInt%uint64(1 / *bordaSamplePercentage) != 0 {
-				log.Trace("DeviceID not being sampled")
-			}
-
-			values := map[string]float64{}
-			if failure != nil {
-				values["error_count"] = 1
-			} else {
-				values["success_count"] = 1
-			}
-			reportErr := reportToBorda(values, ctx)
-			if reportErr != nil {
-				log.Errorf("Error reporting error to borda: %v", reportErr)
-			}
-		})
+		borda.Enable(*bordaReportInterval, *bordaSamplePercentage)
 	}
 
 	// Set up a blacklist
