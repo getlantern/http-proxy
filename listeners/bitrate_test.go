e50f4d903bef2f3a64f622b32056b4644e51e841
Ulysses Aalto <uaalto@getlantern.org>
2016-05-31 18:26:21 +0200

    throttling benchmark

--- listeners/bitrate_test.go
+++ listeners/bitrate_test.go
@@ -2,7 +2,9 @@ package listeners
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"sync"
 	"testing"
 	"time"
 
@@ -82,4 +84,80 @@ Done:
 	}
 
 	assert.Equal(t, bitrateLimit, totalRead, "Read an unexpected number of bytes! Rate limiting is not working")
+}
+
+var onceStd, onceThr sync.Once
+
+func benchSrv(wg *sync.WaitGroup, enableBitrate bool) {
+	wg.Add(1)
+	go func() {
+		var port string
+		if enableBitrate {
+			port = ":9991"
+		} else {
+			port = ":9990"
+		}
+		ln, err := net.Listen("tcp", port)
+		if err != nil {
+			panic(err)
+		}
+		bl := NewBitrateListener(ln, 1024*1024*1024)
+
+		wg.Done()
+
+		for {
+			conn, err := bl.Accept()
+			if err != nil {
+				panic(err)
+			}
+
+			conn.(*bitrateConn).active = enableBitrate
+			go func() {
+				for {
+					b := make([]byte, 512)
+					n, err := conn.Read(b)
+					if err != nil && err != io.EOF {
+						panic(err)
+					}
+					if n == 0 {
+						break
+					}
+				}
+			}()
+		}
+	}()
+}
+
+func BenchmarkStandardReader(b *testing.B) {
+	var wg sync.WaitGroup
+	onceStd.Do(func() { benchSrv(&wg, false) })
+	wg.Wait()
+
+	conn, err := net.Dial("tcp", "127.0.0.1:9990")
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 1024*1024*1024)
+	for i := range buf {
+		buf[i] = '#'
+	}
+	conn.Write(buf)
+}
+
+func BenchmarkThrottledReader(b *testing.B) {
+	var wg sync.WaitGroup
+	onceThr.Do(func() { benchSrv(&wg, true) })
+	wg.Wait()
+
+	conn, err := net.Dial("tcp", "127.0.0.1:9991")
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 1024*1024*1024)
+	for i := range buf {
+		buf[i] = '#'
+	}
+	conn.Write(buf)
 }
