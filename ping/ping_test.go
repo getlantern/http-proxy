2385d5d2c263987d625c066c828af9f8fcd775e3
Ox Cart <ox.to.a.cart@gmail.com>
2016-10-04 12:33:45 -0500

    Added ability to ping specific URLs

--- ping/ping_test.go
+++ ping/ping_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/getlantern/http-proxy-lantern/common"
 	"github.com/getlantern/http-proxy/filters"
@@ -23,6 +24,7 @@ func TestBypass(t *testing.T) {
 	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
 	n := &next{}
 	err := filter.Apply(w, req, n.do)
+	assert.True(t, n.called)
 	assert.Equal(t, errNext, err)
 }
 
@@ -31,10 +33,74 @@ func TestInvalid(t *testing.T) {
 	w := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
 	req.Header.Set(common.PingHeader, "invalid")
-	err := filter.Apply(w, req, filters.Stop)
+	n := &next{}
+	err := filter.Apply(w, req, n.do)
+	assert.False(t, n.called)
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusBadRequest, w.Result().StatusCode)
 	}
+}
+
+func TestGoodURL(t *testing.T) {
+	origTimingExpiration := timingExpiration
+	defer func() {
+		timingExpiration = origTimingExpiration
+	}()
+	timingExpiration = 5 * time.Second
+
+	goodURL := "https://www.google.com/humans.txt"
+	badURL := "https://www.google.com/unknown.txt"
+	filter := New()
+	statusCode, badTS := doTestURL(t, filter, badURL)
+	if !assert.Equal(t, http.StatusNotFound, statusCode) {
+		return
+	}
+
+	statusCode, firstTS := doTestURL(t, filter, goodURL)
+	if !assert.Equal(t, http.StatusOK, statusCode) {
+		return
+	}
+	assert.NotEqual(t, badTS, firstTS, "Bad timing should not have been cached")
+
+	statusCode, secondTS := doTestURL(t, filter, goodURL)
+	if !assert.Equal(t, http.StatusOK, statusCode) {
+		return
+	}
+	assert.Equal(t, firstTS, secondTS, "Should have used cached timing on 2nd request")
+
+	time.Sleep(timingExpiration)
+	statusCode, thirdTS := doTestURL(t, filter, goodURL)
+	if !assert.Equal(t, http.StatusOK, statusCode) {
+		return
+	}
+	assert.NotEqual(t, secondTS, thirdTS, "Should have gotten new timing on 3rd request")
+
+	time.Sleep(timingExpiration * 2)
+	pingFilter := filter.(*pingMiddleware)
+	pingFilter.urlTimingsMx.RLock()
+	defer pingFilter.urlTimingsMx.RUnlock()
+	assert.Empty(t, pingFilter.urlTimings)
+}
+
+func doTestURL(t *testing.T, filter filters.Filter, url string) (statusCode int, ts string) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
+	req.Header.Set(common.PingURLHeader, url)
+	n := &next{}
+	err := filter.Apply(w, req, n.do)
+	assert.False(t, n.called)
+	if assert.NoError(t, err) {
+		resp := w.Result()
+		statusCode = resp.StatusCode
+		if resp.StatusCode == http.StatusOK {
+			n, _ := io.Copy(ioutil.Discard, w.Result().Body)
+			assert.EqualValues(t, 0, n)
+			ts = resp.Header.Get(common.PingTSHeader)
+			assert.NotEmpty(t, ts)
+		}
+	}
+
+	return
 }
 
 func TestSmall(t *testing.T) {
@@ -54,7 +120,9 @@ func testSize(t *testing.T, size string, mult int) {
 	w := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
 	req.Header.Set(common.PingHeader, size)
-	err := filter.Apply(w, req, filters.Stop)
+	n := &next{}
+	err := filter.Apply(w, req, n.do)
+	assert.False(t, n.called)
 	if assert.NoError(t, err) {
 		resp := w.Result()
 		if assert.Equal(t, http.StatusOK, resp.StatusCode) {
@@ -69,5 +137,6 @@ type next struct {
 }
 
 func (n *next) do() error {
+	n.called = true
 	return errNext
 }
