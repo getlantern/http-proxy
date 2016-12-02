825371dd6d273dc37d7ea3a5b46ed397872176a3
Ox Cart <ox.to.a.cart@gmail.com>
2016-12-02 10:16:12 -0600

    Tracking protocol for site accesses in GA

--- analytics/analytics_test.go
+++ analytics/analytics_test.go
@@ -7,6 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var portsAndProtos = map[string]string{
+	"80":   "http",
+	"443":  "https",
+	"5002": "other",
+	"0":    "unknown",
+	"":     "unknown",
+}
+
 func TestNormalizeSite(t *testing.T) {
 	am := New(&Options{
 		TrackingID:       "12345",
@@ -14,9 +22,12 @@ func TestNormalizeSite(t *testing.T) {
 	})
 	addrs, err := net.LookupHost("ats1.member.vip.ne1.yahoo.com")
 	if assert.NoError(t, err, "Should have been able to resolve yahoo.com") {
-		normalized := am.(*analyticsMiddleware).normalizeSite(addrs[0])
-		assert.Len(t, normalized, 3, "Should have gotten two sites")
-		assert.Equal(t, "ats1.member.vip.ne1.yahoo.com", normalized[1])
-		assert.Equal(t, "/generated/yahoo.com", normalized[2])
+		for port, proto := range portsAndProtos {
+			normalized := am.(*analyticsMiddleware).normalizeSite(addrs[0], port)
+			assert.Len(t, normalized, 4, "Should have gotten two sites and one protocol")
+			assert.Equal(t, "ats1.member.vip.ne1.yahoo.com", normalized[1])
+			assert.Equal(t, "/generated/yahoo.com", normalized[2])
+			assert.Equal(t, "/protocol/"+proto, normalized[3])
+		}
 	}
 }
