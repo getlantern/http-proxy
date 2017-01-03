1b7ad3d143a9c40c0168dede34b1a8c78f0332da
Ox Cart <ox.to.a.cart@gmail.com>
2017-01-02 21:53:49 -0600

    Pinging with icmp

--- ping/ping.go
+++ ping/ping.go
@@ -6,129 +6,218 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"runtime"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/getlantern/go-ping"
 	"github.com/getlantern/golog"
-
-	"github.com/getlantern/http-proxy/filters"
-
 	"github.com/getlantern/http-proxy-lantern/common"
-	"github.com/getlantern/http-proxy-lantern/metrics"
+	"github.com/getlantern/http-proxy/filters"
+)
+
+const (
+	pingInterval = 5 * time.Minute
 )
 
 var (
 	log = golog.LoggerFor("http-proxy-lantern.ping")
 
-	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-	// Data is 1 KB of random data
-	data []byte
+	// Reasonable assumptions about stats until we know more from running checks
+	defaultStats = &stats{
+		rtt: 10 * time.Millisecond,
+		plr: 0,
+	}
 )
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-
-	data = []byte(randStringRunes(1024))
-	data[1023] = '\n'
+type stats struct {
+	origin string
+	rtt    time.Duration
+	plr    float64
 }
 
-// randStringRunes generates a random string of the given length.
-// Taken from http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang.
-func randStringRunes(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
-}
-
-// pingMiddleware intercepts ping requests and returns some random data
 type pingMiddleware struct {
-	smallResponseTime  metrics.MovingAverage
-	mediumResponseTime metrics.MovingAverage
-	largeResponseTime  metrics.MovingAverage
-	timingExpiration   time.Duration
-	urlTimings         map[string]*urlTiming
-	urlTimingsMx       sync.RWMutex
-}
-
-func New(timingExpiration time.Duration) filters.Filter {
-	if timingExpiration <= 0 {
-		timingExpiration = defaultTimingExpiration
-	}
-	pm := &pingMiddleware{
-		smallResponseTime:  metrics.NewMovingAverage(),
-		mediumResponseTime: metrics.NewMovingAverage(),
-		largeResponseTime:  metrics.NewMovingAverage(),
-		timingExpiration:   timingExpiration,
-		urlTimings:         make(map[string]*urlTiming),
-	}
-	go pm.logTimings()
-	go pm.cleanupExpiredTimings()
-	return pm
+	pinger        *ping.Pinger
+	statsByOrigin map[string]*stats
+	httpClient    *http.Client
+	mx            sync.RWMutex
 }
 
 func (pm *pingMiddleware) Apply(w http.ResponseWriter, req *http.Request, next filters.Next) error {
 	log.Trace("In ping")
 	pingSize := req.Header.Get(common.PingHeader)
 	pingURL := req.Header.Get(common.PingURLHeader)
-	if pingSize == "" && pingURL == "" {
+	pingOrigin := req.Header.Get(common.PingOriginHeader)
+	if pingSize == "" && pingURL == "" && pingOrigin == "" {
 		log.Trace("Bypassing ping")
 		return next()
 	}
 	log.Trace("Processing ping")
 
+	if pingOrigin == "" && pingURL != "" {
+		parsed, err := url.Parse(pingURL)
+		if err != nil {
+			pingOrigin = parsed.Host
+		}
+	}
+
+	pm.mx.RLock()
+	statsByOrigin := pm.statsByOrigin
+	pm.mx.RUnlock()
+
+	var s *stats
+	// Go through subsub.sub.domain.tld, stripping away subdomains until we get
+	// a result or run out of domain.
+	parts := strings.Split(strings.ToLower(pingOrigin), ".")
+	for {
+		if len(parts) < 2 {
+			break
+		}
+		origin := strings.Join(parts, ".")
+		log.Debugf("Checking %v", origin)
+		s = statsByOrigin[origin]
+		if s != nil {
+			log.Debugf("Got data for %v", origin)
+			break
+		}
+		parts = parts[1:]
+	}
+
+	if s == nil {
+		// Use googlevideo.com by default
+		s = statsByOrigin["googlevideo.com"]
+	}
+
+	if s == nil {
+		s = defaultStats
+	}
+
 	if pingURL != "" {
-		return pm.urlPing(w, pingURL)
+		// This is an old-style ping, simulate latency by sleeping
+		time.Sleep(s.rtt * 50)
 	}
-
-	var size int
-	var ma metrics.MovingAverage
-	switch pingSize {
-	case "small":
-		size = 1
-		ma = pm.smallResponseTime
-	case "medium":
-		size = 100
-		ma = pm.mediumResponseTime
-	case "large":
-		size = 10000
-		ma = pm.largeResponseTime
-	default:
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Invalid ping size %v\n", pingSize)
-		return filters.Stop()
-	}
-
-	return pm.cannedPing(w, ma, size)
-}
-
-func (pm *pingMiddleware) cannedPing(w http.ResponseWriter, ma metrics.MovingAverage, size int) error {
-	start := time.Now()
-	w.WriteHeader(200)
-	for i := 0; i < size; i++ {
-		w.Write(data)
-	}
-	// Flush to the client to make sure we're getting a comprehensive timing
-	w.(http.Flusher).Flush()
-	delta := time.Now().Sub(start)
-	ma.Update(delta.Nanoseconds() / 1000)
-
+	w.Header().Set(common.PingRTTHeader, fmt.Sprint(s.rtt))
+	w.Header().Set(common.PingPLRHeader, fmt.Sprint(s.plr))
+	w.WriteHeader(http.StatusOK)
 	return filters.Stop()
 }
 
-func (pm *pingMiddleware) logTimings() {
-	for {
-		time.Sleep(1 * time.Minute)
-		now := time.Now()
-		msg := fmt.Sprintf(`**** Average Ping Response Times in µs, moving average (1 min, 5 min, 15 min) ****
-%v Small      (1 KB) - %v
-%v Medium   (100 KB) - %v
-%v Large (10,000 KB) - %v
-`, now, pm.smallResponseTime,
-			now, pm.mediumResponseTime,
-			now, pm.largeResponseTime)
-		log.Debug(msg)
+func New() (filters.Filter, error) {
+	// Run privileged on Windows where this doesn't require root and where udp
+	// encapsulation doesn't work
+	privileged := runtime.GOOS == "windows"
+	pinger, err := ping.NewPinger(privileged)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to initialize pinger: %v", err)
+	}
+
+	pm := &pingMiddleware{
+		pinger:        pinger,
+		statsByOrigin: make(map[string]*stats, len(origins)),
+		httpClient: &http.Client{
+			Transport: &http.Transport{
+				DisableKeepAlives: true,
+			},
+		},
+	}
+	go pm.ping()
+
+	return pm, nil
+}
+
+func (pm *pingMiddleware) ping() {
+	resultCh := make(chan *stats, len(origins))
+	for _, origin := range origins {
+		go pm.pingOrigin(origin, resultCh)
+	}
+
+	for s := range resultCh {
+		log.Debugf("ping results for %v  rtt: %4dms  plr: %3.2f%%", s.origin, s.rtt.Nanoseconds()/1000000, s.plr)
+		pm.mx.Lock()
+		// Copy stats map
+		statsByOrigin := make(map[string]*stats, len(origins))
+		for key, value := range pm.statsByOrigin {
+			statsByOrigin[key] = value
+		}
+		// Update stats for this origin
+		statsByOrigin[s.origin] = s
+		pm.statsByOrigin = statsByOrigin
+		pm.mx.Unlock()
 	}
 }
+
+func (pm *pingMiddleware) doUpdateTimings(resultCh chan *stats) {
+	// With current number of origins and settings, each loop of this will take
+	// between 10 and 30 minutes, so there's no need to have an artificial pause.
+
+}
+
+func (pm *pingMiddleware) pingOrigin(origin string, resultCh chan *stats) {
+	for {
+		log.Debugf("pinging %v", origin)
+		st, err := pm.pinger.Ping(origin,
+			// Ping 100 times to get a decent resolution
+			100,
+			// Ping every 200 milliseconds, which is the lowest amount that OS ping
+			// command allows without root (to avoid ICMP flooding)
+			200*time.Millisecond,
+			// Don't let pinging take more than 1 minute total
+			1*time.Minute)
+		if err != nil {
+			log.Errorf("Error pinging %v: %v", origin, err)
+		} else {
+			resultCh <- &stats{
+				origin: origin,
+				rtt:    st.AvgRtt,
+				plr:    st.PacketLoss,
+			}
+		}
+		time.Sleep(randomize(pingInterval))
+	}
+}
+
+// adds randomization to make requests less distinguishable on the network.
+func randomize(d time.Duration) time.Duration {
+	return time.Duration((d.Nanoseconds() / 2) + rand.Int63n(d.Nanoseconds()))
+}
+
+var (
+	// Hardcoded list of origins to ping, based on top sites reported in borda
+	origins = []string{
+		"facebook.com",
+		"fbcdn.net",
+		"mtalk.google.com",
+		"clients1.google.com",
+		"clients2.google.com",
+		"clients3.google.com",
+		"clients4.google.com",
+		"clients5.google.com",
+		"clients6.google.com",
+		"safebrowsing.google.com",
+		"google.com",
+		"google-analytics.com",
+		"gstatic.com",
+		"i.ytimg.com",
+		"i1.ytimg.com",
+		"i9.ytimg.com",
+		"s.ytimg.com",
+		"youtube.com",
+		"googlevideo.com",
+		"twitter.com",
+		"abs.twimg.com",
+		"pbs.twimg.com",
+		"mail.yahoo.com",
+		"yahoo.com",
+		"149.154.167.91",
+		"tumblr.com",
+		"dropbox.com",
+		"fb-s-a-a.akamaihd.net",
+		"fb-s-b-a.akamaihd.net",
+		"fb-s-c-a.akamaihd.net",
+		"fb-s-d-a.akamaihd.net",
+		"fbexternal-a.akamaihd.net",
+		"steamcommunity-a.akamaihd.net",
+	}
+)
--- ping/ping_test.go
+++ ping/ping_test.go
@@ -2,16 +2,14 @@ package ping
 
 import (
 	"errors"
-	"io"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/getlantern/http-proxy-lantern/common"
-	"github.com/getlantern/http-proxy/filters"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,112 +18,61 @@ var (
 )
 
 func TestBypass(t *testing.T) {
-	filter := New(0)
+	filter, err := New()
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer func() {
+		filter.(*pingMiddleware).pinger.Close()
+	}()
+
 	w := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
 	n := &next{}
-	err := filter.Apply(w, req, n.do)
+	err = filter.Apply(w, req, n.do)
 	assert.True(t, n.wasCalled())
 	assert.Equal(t, errNext, err)
 }
 
-func TestInvalid(t *testing.T) {
-	filter := New(0)
+func TestGood(t *testing.T) {
+	filter, err := New()
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer func() {
+		filter.(*pingMiddleware).pinger.Close()
+	}()
+
+	// Give the filter some time to pick up new timings
+	time.Sleep(5 * time.Minute)
+
 	w := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
-	req.Header.Set(common.PingHeader, "invalid")
+	req.Header.Set(common.PingOriginHeader, "FaceBook.com")
 	n := &next{}
-	err := filter.Apply(w, req, n.do)
+	err = filter.Apply(w, req, n.do)
 	assert.False(t, n.wasCalled())
-	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusBadRequest, w.Result().StatusCode)
-	}
-}
-
-func TestGoodURL(t *testing.T) {
-	timingExpiration := 5 * time.Second
-	goodURL := "https://www.google.com/humans.txt"
-	badURL := "https://www.google.com/unknown.txt"
-	filter := New(timingExpiration)
-	statusCode, badTS := doTestURL(t, filter, badURL)
-	if !assert.Equal(t, http.StatusNotFound, statusCode) {
+	if !assert.NoError(t, err) {
 		return
 	}
-
-	statusCode, firstTS := doTestURL(t, filter, goodURL)
-	if !assert.Equal(t, http.StatusOK, statusCode) {
+	resp := w.Result()
+	if !assert.Equal(t, http.StatusOK, resp.StatusCode) {
 		return
 	}
-	assert.NotEqual(t, badTS, firstTS, "Bad timing should not have been cached")
-
-	statusCode, secondTS := doTestURL(t, filter, goodURL)
-	if !assert.Equal(t, http.StatusOK, statusCode) {
+	defer resp.Body.Close()
+	rtt, err := time.ParseDuration(resp.Header.Get(common.PingRTTHeader))
+	if !assert.NoError(t, err) {
 		return
 	}
-	assert.Equal(t, firstTS, secondTS, "Should have used cached timing on 2nd request")
-
-	time.Sleep(timingExpiration * 2)
-	statusCode, thirdTS := doTestURL(t, filter, goodURL)
-	if !assert.Equal(t, http.StatusOK, statusCode) {
+	plr, err := strconv.ParseFloat(resp.Header.Get(common.PingPLRHeader), 64)
+	if !assert.NoError(t, err) {
 		return
 	}
-	assert.NotEqual(t, secondTS, thirdTS, "Should have gotten new timing on 3rd request")
-
-	time.Sleep(timingExpiration * 2)
-	pingFilter := filter.(*pingMiddleware)
-	pingFilter.urlTimingsMx.RLock()
-	defer pingFilter.urlTimingsMx.RUnlock()
-	assert.Empty(t, pingFilter.urlTimings)
-}
-
-func doTestURL(t *testing.T, filter filters.Filter, url string) (statusCode int, ts string) {
-	w := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
-	req.Header.Set(common.PingURLHeader, url)
-	n := &next{}
-	err := filter.Apply(w, req, n.do)
-	assert.False(t, n.wasCalled())
-	if assert.NoError(t, err) {
-		resp := w.Result()
-		statusCode = resp.StatusCode
-		if resp.StatusCode == http.StatusOK {
-			n, _ := io.Copy(ioutil.Discard, w.Result().Body)
-			assert.EqualValues(t, 0, n)
-			ts = resp.Header.Get(common.PingTSHeader)
-			assert.NotEmpty(t, ts)
-		}
-	}
-
-	return
-}
-
-func TestSmall(t *testing.T) {
-	testSize(t, "small", 1)
-}
-
-func TestMedium(t *testing.T) {
-	testSize(t, "medium", 100)
-}
-
-func TestLarge(t *testing.T) {
-	testSize(t, "large", 10000)
-}
-
-func testSize(t *testing.T, size string, mult int) {
-	filter := New(0)
-	w := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "http://doesntmatter.domain", nil)
-	req.Header.Set(common.PingHeader, size)
-	n := &next{}
-	err := filter.Apply(w, req, n.do)
-	assert.False(t, n.wasCalled())
-	if assert.NoError(t, err) {
-		resp := w.Result()
-		if assert.Equal(t, http.StatusOK, resp.StatusCode) {
-			n, _ := io.Copy(ioutil.Discard, w.Result().Body)
-			assert.EqualValues(t, mult*len(data), n)
-		}
-	}
+	log.Debug(rtt)
+	log.Debug(plr)
+	assert.True(t, rtt > 0)
+	assert.True(t, plr >= 0)
+	assert.NotEqual(t, defaultStats.rtt, rtt, "Should have gotten non-default rtt")
 }
 
 type next struct {
