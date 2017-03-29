7c989e0fadb2b25c089c3ab4c3ce212d97cf2b4c
Ox Cart <ox.to.a.cart@gmail.com>
2017-03-29 15:43:20 -0500

    Improved BBR package comment

--- bbr/bbr.go
+++ bbr/bbr.go
@@ -2,14 +2,15 @@
 //
 // Bandwidth estimates are provided to clients following the below protocol:
 //
-// 1. On every inbound, we interrogate BBR congestion control parameters to
-//    determine the estimated bandwidth, extrapolate this using a linear
-//    estimation based on how much data has actually been transferred on the
-//    connection and then maintain an exponential moving average (EMA) of these
-//    estimates per remote (client) IP.
-// 2. If a client includes HTTP header X-BBR: <anything>, we include header
-//    X-BBR-ABE: <EMA bandwidth in Mbps>.
-// 3. If a client includes HTTP header X-BBR: clear, we clear stored estimate
+// 1. On every inbound connection, we interrogate BBR congestion control
+//    parameters to determine the estimated bandwidth, extrapolate this to what
+//    we would expected for a 2.5 MB transfer using a linear estimation based on
+//    how much data has actually been transferred on the connection and then
+//    maintain an exponential moving average (EMA) of these estimates per remote
+//    (client) IP.
+// 2. If a client includes HTTP header "X-BBR: <anything>", we include header
+//    X-BBR-ABE: <EMA bandwidth in Mbps> in the HTTP response.
+// 3. If a client includes HTTP header "X-BBR: clear", we clear stored estimate
 //    data for the client's IP.
 //
 package bbr
