dfcc1d4f831242669310d06a6811a295108f2774
Ox Cart <ox.to.a.cart@gmail.com>
2017-07-09 20:23:37 -0500

    Updated devicefilter to use new filters API

--- devicefilter/devicefilter.go
+++ devicefilter/devicefilter.go
@@ -1,17 +1,17 @@
 package devicefilter
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"time"
 
-	"github.com/gorilla/context"
-
 	"github.com/getlantern/golog"
+	"github.com/getlantern/proxy"
+	"github.com/getlantern/proxy/filters"
 
-	"github.com/getlantern/http-proxy/filters"
 	"github.com/getlantern/http-proxy/listeners"
 
 	"github.com/getlantern/http-proxy-lantern/blacklist"
@@ -52,7 +52,7 @@ func NewPre(df *redis.DeviceFetcher, throttleConfig throttle.Config, fasttrackDo
 	}
 }
 
-func (f *deviceFilterPre) Apply(w http.ResponseWriter, req *http.Request, next filters.Next) error {
+func (f *deviceFilterPre) Apply(ctx context.Context, req *http.Request, next filters.Next) (*http.Response, error) {
 	if log.IsTraceEnabled() {
 		reqStr, _ := httputil.DumpRequest(req, true)
 		log.Tracef("DeviceFilter Middleware received request:\n%s", reqStr)
@@ -61,12 +61,12 @@ func (f *deviceFilterPre) Apply(w http.ResponseWriter, req *http.Request, next f
 	// If we're requesting a whitelisted domain, don't count it towards the
 	// bandwidth cap.
 	if f.fasttrackDomains.Whitelisted(req) {
-		return next()
+		return next(ctx, req)
 	}
 
 	// Attached the uid to connection to report stats to redis correctly
 	// "conn" in context is previously attached in server.go
-	wc := context.Get(req, "conn").(listeners.WrapConn)
+	wc := proxy.DownstreamConn(ctx).(listeners.WrapConn)
 
 	lanternDeviceID := req.Header.Get(common.DeviceIdHeader)
 
@@ -77,12 +77,16 @@ func (f *deviceFilterPre) Apply(w http.ResponseWriter, req *http.Request, next f
 			common.DeviceIdHeader, req.RemoteAddr, req.Host)
 	} else {
 		if f.throttleConfig != nil {
+			resp, err := next(ctx, req)
 			// Throttling enabled
 			u := usage.Get(lanternDeviceID)
 			if u == nil {
 				// Eagerly request device ID data to Redis and store it in usage
 				f.deviceFetcher.RequestNewDeviceUsage(lanternDeviceID)
-				return next()
+				return resp, err
+			}
+			if err != nil {
+				return resp, err
 			}
 			uMiB := u.Bytes / (1024 * 1024)
 			// Encode usage information in a header. The header is expected to follow
@@ -95,14 +99,15 @@ func (f *deviceFilterPre) Apply(w http.ResponseWriter, req *http.Request, next f
 			// <asof> is the 64-bit signed integer representing seconds since a custom
 			// epoch (00:00:00 01/01/2016 UTC).
 			threshold, rate := f.throttleConfig.ThresholdAndRateFor(lanternDeviceID, u.CountryCode)
-			w.Header().Set(common.XBQHeader, fmt.Sprintf("%d/%d/%d", uMiB, threshold/(1024*1024), int64(u.AsOf.Sub(epoch).Seconds())))
+			resp.Header.Set(common.XBQHeader, fmt.Sprintf("%d/%d/%d", uMiB, threshold/(1024*1024), int64(u.AsOf.Sub(epoch).Seconds())))
 			if u.Bytes > threshold {
 				wc.ControlMessage("throttle", lanternlisteners.ThrottleRate(rate))
 			}
+			return resp, err
 		}
 	}
 
-	return next()
+	return next(ctx, req)
 }
 
 func NewPost(bl *blacklist.Blacklist) filters.Filter {
@@ -111,10 +116,10 @@ func NewPost(bl *blacklist.Blacklist) filters.Filter {
 	}
 }
 
-func (f *deviceFilterPost) Apply(w http.ResponseWriter, req *http.Request, next filters.Next) error {
+func (f *deviceFilterPost) Apply(ctx context.Context, req *http.Request, next filters.Next) (*http.Response, error) {
 	// For privacy, delete the DeviceId header before passing it along
 	req.Header.Del(common.DeviceIdHeader)
 	ip, _, _ := net.SplitHostPort(req.RemoteAddr)
 	f.bl.Succeed(ip)
-	return next()
+	return next(ctx, req)
 }
