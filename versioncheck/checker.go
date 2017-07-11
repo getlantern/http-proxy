05d6800eb2fe2d4a407470be25a3bc4acc1ea2be
Ox Cart <ox.to.a.cart@gmail.com>
2017-07-11 11:44:04 -0500

    Added debug logging to checker_test

--- versioncheck/checker.go
+++ versioncheck/checker.go
@@ -183,10 +183,13 @@ func (c *VersionChecker) redirectOnConnect(ctx context.Context, req *http.Reques
 		return nil, ctx, err
 	}
 
+	log.Debug("Waiting")
 	// Make sure the application sent something and started waiting for the
 	// response.
 	var buf [1]byte
 	_, _ = conn.Read(buf[:])
+
+	log.Debug("Waited")
 
 	// Send the actual response to application.
 	return &http.Response{
