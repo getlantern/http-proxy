20257eb402767e5273d8e9e0948904bdbe447e7d
Ox Cart <ox.to.a.cart@gmail.com>
2018-06-16 22:40:52 -0500

    onlyDeployTo now stops http-proxy before scp

--- onlyDeployTo.bash
+++ onlyDeployTo.bash
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
+ip=$1
+
+function cleanup() {
+  echo "Starting http-proxy-lantern"
+  ssh lantern@$ip -t "sudo service http-proxy start" || die "Could not start http-proxy"
+}
+
 function die() {
   echo "$@"
+  cleanup
   exit 1
 }
 
-ip=$1
-
 echo "Disabling auto-update on $ip"
 ssh lantern@$ip -t "sudo crontab -l | perl -p -e 's/^(.*update_proxy.bash.*)/#\1/g' | sudo crontab -" || die "Could not disable auto-updates"
+
+echo "Stopping http-proxy-lantern to allow upload"
+ssh lantern@$ip -t "sudo service http-proxy stop"
 
 echo "Uploading http-proxy-lantern"
 scp dist/http-proxy lantern@$ip:http-proxy || die "Could not copy binary"
@@ -17,5 +26,4 @@ scp dist/http-proxy lantern@$ip:http-proxy || die "Could not copy binary"
 echo "Calling setcap on http-proxy"
 ssh lantern@$ip -t "sudo setcap 'cap_net_bind_service=+ep' /home/lantern/http-proxy" || die "Error calling setcap on http-proxy"
 
-echo "Restarting http-proxy-lantern"
-ssh lantern@$ip -t "sudo service http-proxy restart" || die "Could not restart"
+cleanup
