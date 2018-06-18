de1a9e5cbd3765064f93ec5a456b6ccdc6ecc3b6
Myles Horton <myles@getlantern.org>
2018-06-18 13:51:39 -0700

    more debugging for tls connections

--- tlslistener/tlslistener.go
+++ tlslistener/tlslistener.go
@@ -6,20 +6,26 @@ import (
 	"crypto/tls"
 	"net"
 
+	"github.com/getlantern/golog"
 	"github.com/getlantern/tlsdefaults"
 )
 
+// Wrap wraps the specified listener in our default TLS listener.
 func Wrap(wrapped net.Listener, keyFile string, certFile string) (net.Listener, error) {
 	cfg, err := tlsdefaults.BuildListenerConfig(wrapped.Addr().String(), keyFile, certFile)
 	if err != nil {
 		return nil, err
 	}
-	return &tlslistener{wrapped, cfg}, nil
+
+	listener := &tlslistener{wrapped, cfg, golog.LoggerFor("lantern-proxy-tlslistener")}
+	cfg.GetConfigForClient = listener.debugClientHello
+	return listener, nil
 }
 
 type tlslistener struct {
 	wrapped net.Listener
 	cfg     *tls.Config
+	log     golog.Logger
 }
 
 func (l *tlslistener) Accept() (net.Conn, error) {
@@ -36,6 +42,11 @@ func (l *tlslistener) Addr() net.Addr {
 
 func (l *tlslistener) Close() error {
 	return l.wrapped.Close()
+}
+
+func (l *tlslistener) debugClientHello(info *tls.ClientHelloInfo) (*tls.Config, error) {
+	l.log.Debugf("Cipher suites from client %v for server suites %v "+info.Conn.RemoteAddr().String()+"->"+info.Conn.LocalAddr().String(), info.CipherSuites, l.cfg.CipherSuites)
+	return nil, nil
 }
 
 type tlsconn struct {
---
+++ tlslistener/tlslistener_test.go
@@ -0,0 +1,25 @@
+package tlslistener
+
+import (
+	"crypto/tls"
+	"net"
+	"testing"
+
+	"github.com/getlantern/golog"
+)
+
+func TestWrap(t *testing.T) {
+
+	tlsl := &tlslistener{
+		cfg: &tls.Config{},
+		log: golog.LoggerFor("tlslistener-test"),
+	}
+
+	_, client := net.Pipe()
+	info := &tls.ClientHelloInfo{
+		CipherSuites: []uint16{tls.TLS_RSA_WITH_RC4_128_SHA},
+		Conn:         client,
+	}
+
+	tlsl.debugClientHello(info)
+}
