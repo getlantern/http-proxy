f395b5344f68fe6e2479b6b487ca256646e83bf2
Myles Horton <myles@getlantern.org>
2018-06-22 12:35:46 -0700

    logic fix

--- tlslistener/tlslistener.go
+++ tlslistener/tlslistener.go
@@ -52,10 +52,10 @@ var standardClientSuites1 = []uint16{49199, 49200, 49195, 49196, 52392, 52393, 4
 var standardClientSuites2 = []uint16{52392, 52393, 49199, 49200, 49195, 49196, 49171, 49161, 49172, 49162, 156, 157, 47, 53, 49170, 10}
 
 func (l *tlslistener) debugClientHello(info *tls.ClientHelloInfo) (*tls.Config, error) {
-	if !testEq(standardClientSuites1, info.CipherSuites) || !testEq(standardClientSuites2, info.CipherSuites) {
-		l.log.Debugf("Unexpected suites from client %v: %v, %v", info.Conn.RemoteAddr(), info.CipherSuites, l.suiteStrings(info))
+	if testEq(standardClientSuites1, info.CipherSuites) || testEq(standardClientSuites2, info.CipherSuites) {
+		return nil, nil
 	}
-
+	l.log.Debugf("Unexpected suites from client %v: %v, %v", info.Conn.RemoteAddr(), info.CipherSuites, l.suiteStrings(info))
 	return nil, nil
 }
 
