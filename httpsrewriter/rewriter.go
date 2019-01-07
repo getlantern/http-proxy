777071a70c8cb747a7544e8c7abf9c3e532c2096
Joesis <joesis@getlantern.org>
2019-01-06 21:48:47 -0800

    split configserverfilter to httpsrewriter and requestModifier

---
+++ httpsrewriter/rewriter.go
@@ -0,0 +1,71 @@
+// Package httpsrewriter rewrite the scheme and dest port of requests to HTTPS
+// for the configured domains. It can also wrap a dialer to dial TLS. See
+// https://github.com/getlantern/config-server/issues/4 for its usage.
+
+package httpsrewriter
+
+import (
+	"context"
+	"crypto/tls"
+	"net"
+	"net/http"
+
+	"github.com/getlantern/golog"
+	"github.com/getlantern/proxy/filters"
+)
+
+var log = golog.LoggerFor("httpsRewritter")
+
+type Dial func(ctx context.Context, network, address string) (net.Conn, error)
+
+type Rewriter struct {
+	Domains []string
+}
+
+func (f *Rewriter) Dialer(d Dial) Dial {
+	return func(ctx context.Context, network, address string) (net.Conn, error) {
+		conn, err := d(ctx, network, address)
+		if err != nil {
+			return conn, err
+		}
+		if matched := in(address, f.Domains); matched != "" {
+			conn = tls.Client(conn, &tls.Config{ServerName: matched})
+			log.Debugf("Added TLS to connection to %s", address)
+		}
+		return conn, err
+	}
+}
+
+func (f *Rewriter) Apply(ctx filters.Context, req *http.Request, next filters.Next) (*http.Response, filters.Context, error) {
+	f.RewriteIfNecessary(req)
+	return next(ctx, req)
+}
+
+func (f *Rewriter) RewriteIfNecessary(req *http.Request) {
+	if req.Method == "CONNECT" {
+		return
+	}
+	if matched := in(req.Host, f.Domains); matched != "" {
+		f.rewrite(matched, req)
+	}
+}
+
+func (f *Rewriter) rewrite(host string, req *http.Request) {
+	// for some reason we forgot, the scheme has to be cleared, rather than set to "https"
+	req.URL.Scheme = ""
+	req.Host = host + ":443"
+}
+
+// in returns the host portion if it's is in the domains list, or returns ""
+func in(hostport string, domains []string) string {
+	host, _, err := net.SplitHostPort(hostport)
+	if err != nil {
+		host = hostport
+	}
+	for _, d := range domains {
+		if host == d {
+			return d
+		}
+	}
+	return ""
+}
--- httpsrewriter/rewriter_test.go
+++ httpsrewriter/rewriter_test.go
@@ -1,12 +1,15 @@
-package configserverfilter
+package httpsrewriter
 
 import (
+	"context"
+	"crypto/tls"
+	"net"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/getlantern/http-proxy-lantern/common"
+	"github.com/getlantern/mockconn"
 	"github.com/getlantern/proxy/filters"
 )
 
@@ -17,12 +20,10 @@ func (h *dummyHandler) Apply(ctx filters.Context, req *http.Request, next filter
 	return next(ctx, req)
 }
 
-func TestModifyRequest(t *testing.T) {
-	fakeToken := "fake-token"
-	dummyClientIP := "1.1.1.1"
-	dummyAddr := dummyClientIP + ":12345"
+func TestRedirect(t *testing.T) {
+	dummyAddr := "a.b.c.d:12345"
 	dummy := &dummyHandler{}
-	chain := filters.Join(New(&Options{fakeToken, []string{"site1.com", "site2.org"}}), dummy)
+	chain := filters.Join(&Rewriter{[]string{"site1.com", "site2.org"}}, dummy)
 
 	req, _ := http.NewRequest("GET", "http://site1.com:80/abc.gz", nil)
 	req.RemoteAddr = dummyAddr
@@ -33,8 +34,6 @@ func TestModifyRequest(t *testing.T) {
 	chain.Apply(ctx, req, next)
 	assert.Equal(t, "", dummy.req.URL.Scheme, "should clear scheme")
 	assert.Equal(t, "site1.com:443", dummy.req.Host, "should use port 443")
-	assert.Equal(t, fakeToken, dummy.req.Header.Get(common.CfgSvrAuthTokenHeader), "should attach token")
-	assert.Equal(t, dummyClientIP, dummy.req.Header.Get(common.CfgSvrClientIPHeader), "should attach client ip")
 
 	for _, method := range []string{"GET", "HEAD", "PUT", "POST", "DELETE", "OPTIONS"} {
 		req, _ = http.NewRequest(method, "http://site2.org/abc.gz", nil)
@@ -42,8 +41,6 @@ func TestModifyRequest(t *testing.T) {
 		chain.Apply(ctx, req, next)
 		assert.Equal(t, "", dummy.req.URL.Scheme, "should clear scheme")
 		assert.Equal(t, "site2.org:443", dummy.req.Host, "should use port 443")
-		assert.Equal(t, fakeToken, dummy.req.Header.Get(common.CfgSvrAuthTokenHeader), "should attach token")
-		assert.Equal(t, dummyClientIP, dummy.req.Header.Get(common.CfgSvrClientIPHeader), "should attach client ip")
 	}
 
 	req, _ = http.NewRequest("CONNECT", "http://site2.org/", nil)
@@ -51,38 +48,37 @@ func TestModifyRequest(t *testing.T) {
 	chain.Apply(ctx, req, next)
 	assert.Equal(t, "http", dummy.req.URL.Scheme, "should not clear scheme")
 	assert.Equal(t, "site2.org", dummy.req.Host, "should remain http")
-	assert.Empty(t, dummy.req.Header.Get(common.CfgSvrAuthTokenHeader), "should not attach token")
-	assert.Empty(t, dummy.req.Header.Get(common.CfgSvrClientIPHeader), "should not attach client ip")
 
 	req, _ = http.NewRequest("GET", "http://site2.org:443/abc.gz", nil)
 	req.RemoteAddr = "bad-addr"
 	chain.Apply(ctx, req, next)
 	assert.Equal(t, "", dummy.req.URL.Scheme, "should clear scheme")
 	assert.Equal(t, "site2.org:443", dummy.req.Host, "should use port 443")
-	assert.Equal(t, fakeToken, dummy.req.Header.Get(common.CfgSvrAuthTokenHeader), "should attach token")
-	assert.Equal(t, "", dummy.req.Header.Get(common.CfgSvrClientIPHeader), "should not attach client ip if remote address is invalid")
 
 	req, _ = http.NewRequest("GET", "http://not-config-server.org/abc.gz", nil)
 	req.RemoteAddr = dummyAddr
 	chain.Apply(ctx, req, next)
 	assert.Equal(t, "http", dummy.req.URL.Scheme, "should not rewrite to https for other sites")
 	assert.Equal(t, "not-config-server.org", dummy.req.Host, "should not use port 443 for other sites")
-	assert.Equal(t, "", dummy.req.Header.Get(common.CfgSvrAuthTokenHeader), "should not attach token for other sites")
-	assert.Equal(t, "", dummy.req.Header.Get(common.CfgSvrClientIPHeader), "should not attach client ip for other sites")
 }
 
-func TestInitializeNoDomains(t *testing.T) {
-	defer func() {
-		r := recover()
-		assert.NotNil(t, r, "should panic when no domains provided")
-	}()
-	New(&Options{AuthToken: "fake-token"})
+func TestDialerConfigServer(t *testing.T) {
+	d := &net.Dialer{}
+	dial := (&Rewriter{[]string{"config.getiantem.org"}}).Dialer(d.DialContext)
+	conn, err := dial(context.Background(), "tcp", "config.getiantem.org:443")
+	assert.NoError(t, err)
+	conn.Close()
 }
 
-func TestInitializeNoAuthToken(t *testing.T) {
-	defer func() {
-		r := recover()
-		assert.NotNil(t, r, "should panic when no token provided")
-	}()
-	New(&Options{Domains: []string{"site1.com", "site2.org"}})
+func TestDialer(t *testing.T) {
+	dummyDial := func(ctx context.Context, net, addr string) (net.Conn, error) {
+		return mockconn.SucceedingDialer([]byte{}).Dial(net, addr)
+	}
+	d := (&Rewriter{[]string{"site1", "site2"}}).Dialer(dummyDial)
+	c, _ := d(context.Background(), "tcp", "site1")
+	_, ok := c.(*tls.Conn)
+	assert.True(t, ok, "should override dialer if site is in list")
+	c, _ = d(context.Background(), "tcp", "other")
+	_, ok = c.(*tls.Conn)
+	assert.False(t, ok, "should not override dialer for other dialers")
 }
