b231a5fb10f088a7cc4729638ff62b067efba3e2
Myles Horton <myles@getlantern.org>
2019-10-20 20:52:22 -0700

    Fix Proxy-Connection header handling

--- http_proxy.go
+++ http_proxy.go
@@ -549,6 +549,14 @@ func (p *Proxy) createFilterChain(bl *blacklist.Blacklist) (filters.Chain, proxy
 				// Only add X-Forwarded-For for certain domains
 				return proxyfilters.AddForwardedFor(ctx, req, next)
 			}
+			// If we're sending traffic directly rather than via a proxy, we want to delete
+			// the Proxy-Connection header and convert it to a Connection header, which
+			// is what the browser expects.
+			pc := req.Header.Get("Proxy-Connection")
+			if pc != "" {
+				req.Header.Set("Connection", pc)
+			}
+			req.Header.Del("Proxy-Connection")
 			return next(ctx, req)
 		}),
 		httpsupgrade.NewHTTPSUpgrade(p.CfgSvrAuthToken),
