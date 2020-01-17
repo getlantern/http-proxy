0340bb32cea3de0a063a9f7f0e0dd2306e98d6bf
Max Bittman <max@npmjs.com>
2020-01-17 10:18:59 -0800

    Handle hex decoding in tlsmasq Wrap function

--- tlsmasq/tlsmasq.go
+++ tlsmasq/tlsmasq.go
@@ -2,6 +2,7 @@ package tlsmasq
 
 import (
 	"crypto/tls"
+	"encoding/hex"
 	"fmt"
 	"net"
 
@@ -9,10 +10,19 @@ import (
 	"github.com/getlantern/tlsmasq/ptlshs"
 )
 
-func Wrap(ll net.Listener, certFile string, keyFile string, proxiedAddr string, secret [52]byte) (net.Listener, error) {
+func Wrap(ll net.Listener, certFile string, keyFile string, proxiedAddr string, secret string) (net.Listener, error) {
+	var secretBytes [52]byte
+	if _secretBytes, decodeErr := hex.DecodeString(secret); decodeErr != nil {
+		return nil, fmt.Errorf(`unable to decode secret string "%v": %v`, secret, decodeErr)
+	} else {
+		if copy(secretBytes[:], _secretBytes) != 52 {
+			return nil, fmt.Errorf(`secret string did not parse to 52 bytes: "%v"`, secret)
+		}
+	}
+
 	cert, keyErr := tls.LoadX509KeyPair(certFile, keyFile)
 	if keyErr != nil {
-		return nil, fmt.Errorf("Unable to load key file for tlsmasq: %v", keyErr)
+		return nil, fmt.Errorf("unable to load key file for tlsmasq: %v", keyErr)
 	}
 
 	dialOrigin := func() (net.Conn, error) { return net.Dial("tcp", proxiedAddr) }
@@ -21,7 +31,7 @@ func Wrap(ll net.Listener, certFile string, keyFile string, proxiedAddr string, 
 	listenerCfg := tlsmasq.ListenerConfig{
 		ProxiedHandshakeConfig: ptlshs.ListenerConfig{
 			DialOrigin: dialOrigin,
-			Secret:     secret,
+			Secret:     secretBytes,
 		},
 		TLSConfig: tlsConfig,
 	}
