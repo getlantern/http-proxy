7e7c15ca34d4dd0864bb0ff895a17a45ffbf33ba
Joesis <joesis@getlantern.org>
2020-04-27 00:39:36 -0700

    also cover prefetching updating local copy of usage data

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -10,13 +10,16 @@ import (
 	"github.com/getlantern/measured"
 	"github.com/stretchr/testify/assert"
 	rclient "gopkg.in/redis.v5"
+
+	"github.com/getlantern/http-proxy-lantern/usage"
 )
 
 func TestReportPeriodically(t *testing.T) {
-	rc, err := newForTesting()
+	rc, err := newTestRedis()
 	assert.NoError(t, err)
 	deviceID := "device12"
 	clientIP := "1.1.1.1"
+	fetcher := NewDeviceFetcher(rc)
 	statsCh := make(chan *statsAndContext, 10000)
 	newStats := func() {
 		statsCh <- &statsAndContext{map[string]interface{}{"deviceid": deviceID, "client_ip": clientIP}, &measured.Stats{RecvTotal: 2, SentTotal: 1}}
@@ -24,6 +27,11 @@ func TestReportPeriodically(t *testing.T) {
 	lookup := &fakeLookup{}
 	go reportPeriodically(lookup, rc, time.Millisecond, statsCh)
 
+	fetcher.RequestNewDeviceUsage(deviceID)
+	time.Sleep(10 * time.Millisecond)
+	localCopy := usage.Get(deviceID)
+	assert.Equal(t, "", localCopy.CountryCode)
+	assert.EqualValues(t, 0, localCopy.Bytes)
 	newStats()
 	time.Sleep(10 * time.Millisecond)
 	result := rc.HGetAll("_client:" + deviceID).Val()
@@ -31,6 +39,9 @@ func TestReportPeriodically(t *testing.T) {
 	assert.Equal(t, "1", result["bytesOut"])
 	assert.Equal(t, "", result["countryCode"])
 	assert.True(t, rc.TTL("_client:"+deviceID).Val() > 0, "should have set TTL to the key")
+	localCopy = usage.Get(deviceID)
+	assert.Equal(t, "", localCopy.CountryCode)
+	assert.EqualValues(t, 3, localCopy.Bytes)
 
 	lookup.countryCode = "ir"
 	newStats()
@@ -39,6 +50,9 @@ func TestReportPeriodically(t *testing.T) {
 	assert.Equal(t, "4", result["bytesIn"])
 	assert.Equal(t, "2", result["bytesOut"])
 	assert.Equal(t, "ir", result["countryCode"])
+	localCopy = usage.Get(deviceID)
+	assert.Equal(t, "ir", localCopy.CountryCode)
+	assert.EqualValues(t, 6, localCopy.Bytes)
 
 	lookup.countryCode = ""
 	newStats()
@@ -54,7 +68,7 @@ func (l *fakeLookup) CountryCode(ip net.IP) string {
 	return l.countryCode
 }
 
-func newForTesting() (*rclient.Client, error) {
+func newTestRedis() (*rclient.Client, error) {
 	url := os.Getenv("REDIS_PORT") // If in Wercker
 	if url == "" {
 		url = "redis://localhost:6379"
