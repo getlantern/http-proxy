6d9dcd2d28c34edb69bad2c2850a204df48030da
Willie Forkner <1120829+forkner@users.noreply.github.com>
2020-09-29 12:25:00 -0400

    remove reference to connection/proxyconnection headers in cleanheadersfilter_test
    The behavior being tested appears to have been removed in b76b2db49acc4d1e0cde2a698b76a0f3f95df2f0

--- cleanheadersfilter/cleanheadersfilter_test.go
+++ cleanheadersfilter/cleanheadersfilter_test.go
@@ -13,8 +13,6 @@ func TestWhitelisted(t *testing.T) {
 	filter.stripHeadersIfNecessary(req)
 	assert.Equal(t, "A", req.Header.Get("X-Lantern-A"))
 	assert.Equal(t, "B", req.Header.Get("X-Lantern-B"))
-	assert.Equal(t, "close", req.Header.Get("Connection"))
-	assert.Empty(t, req.Header.Get("Proxy-Connection"))
 	assert.Equal(t, "O", req.Header.Get("Other"))
 }
 
@@ -24,25 +22,13 @@ func TestNotWhitelisted(t *testing.T) {
 	filter.stripHeadersIfNecessary(req)
 	assert.Empty(t, req.Header.Get("X-Lantern-A"))
 	assert.Empty(t, req.Header.Get("X-Lantern-B"))
-	assert.Equal(t, "close", req.Header.Get("Connection"))
-	assert.Empty(t, req.Header.Get("Proxy-Connection"))
 	assert.Equal(t, "O", req.Header.Get("Other"))
-}
-
-func TestNoProxyConnection(t *testing.T) {
-	filter := New().(*filter)
-	req := buildRequest("https://alipay.com/stuff")
-	req.Header.Del("Proxy-Connection")
-	filter.stripHeadersIfNecessary(req)
-	_, found := req.Header["Connection"]
-	assert.False(t, found)
 }
 
 func buildRequest(url string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set("X-Lantern-A", "A")
 	req.Header.Set("x-lantern-b", "B")
-	req.Header.Set("proxy-Connection", "close")
 	req.Header.Set("Other", "O")
 	return req
 }
