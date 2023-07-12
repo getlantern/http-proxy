3dde5550cd2a3d54413894c83eb7f9bec2937282
Myles Horton <myles@getlantern.org>
2023-07-12 11:47:20 -0400

    renamed method for clarity

--- broflake/broflake.go
+++ broflake/broflake.go
@@ -7,10 +7,12 @@ import (
 	"github.com/getlantern/broflake/egress"
 )
 
+// WrapWebSocket wraps the given listener with a WebSocket listener from broflake.
 func WrapWebSocket(ll net.Listener, certPEM string, keyPEM string) (net.Listener, error) {
 	return egress.NewWebSocketListener(context.Background(), ll, certPEM, keyPEM)
 }
 
-func WrapWebTransport(addr, certPEM, keyPEM string) (net.Listener, error) {
+// NewWebTransportListener creates a new listener for WebTransport connections.
+func NewWebTransportListener(addr, certPEM, keyPEM string) (net.Listener, error) {
 	return egress.NewWebTransportListener(context.Background(), addr, certPEM, keyPEM)
 }
