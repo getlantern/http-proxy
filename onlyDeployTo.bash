b71092d9b098e9ddca1c2040f9e709287d74e59d
Adam Fisk <myles@getlantern.org>
2023-11-27 15:09:03 -0500

    shellcheck fixes

--- onlyDeployTo.bash
+++ onlyDeployTo.bash
@@ -12,16 +12,16 @@ fi
 check_is_proxy_server
 
 echo "Disabling auto-update on $ip"
-ssh lantern@$ip -t "sudo crontab -l | perl -p -e 's/^(.*update_proxy.bash.*)/#\1/g' | sudo crontab -" || die "Could not disable auto-updates"
+ssh lantern@"$ip" -t "sudo crontab -l | perl -p -e 's/^(.*update_proxy.bash.*)/#\1/g' | sudo crontab -" || die "Could not disable auto-updates"
 
 echo "Uploading http-proxy-lantern"
-scp dist-bin/http-proxy lantern@$ip:http-proxy.tmp || die "Could not copy binary"
+scp dist-bin/http-proxy lantern@"$ip":http-proxy.tmp || die "Could not copy binary"
 
 echo "Stopping http-proxy-lantern to allow replacing binary"
-ssh lantern@$ip -t "sudo service http-proxy stop" 
+ssh lantern@"$ip" -t "sudo service http-proxy stop"
 
 echo "Replacing binary"
-ssh lantern@$ip -t "sudo cp /home/lantern/http-proxy.tmp /home/lantern/http-proxy" || cleanup "Could not replace binary"
+ssh lantern@"$ip" -t "sudo cp /home/lantern/http-proxy.tmp /home/lantern/http-proxy" || cleanup "Could not replace binary"
 
 setcap_proxy
 
