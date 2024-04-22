1b57ff56e03f7bd6fb483246eb76abeb825e45c0
Allan Guwatudde <guwats10@gmail.com>
2024-04-22 18:34:35 +0300

    Fix tests

--- proxyfilters/blocklocal_test.go
+++ proxyfilters/blocklocal_test.go
@@ -10,54 +10,55 @@ import (
 	"github.com/getlantern/proxy/v3/filters"
 )
 
-type testResolver struct{}
+type testResolver struct {
+	a, b, c, d byte
+}
 
 func (r *testResolver) SplitHostPort(hostport string) (host string, port string, err error) {
 	return net.SplitHostPort(hostport)
 }
 func (r *testResolver) ResolveIPAddr(network string, address string) (*net.IPAddr, error) {
-
 	return &net.IPAddr{
-		IP: net.IPv4(93, 184, 215, 16),
+		IP: net.IPv4(r.a, r.b, r.c, r.d),
 	}, nil
 }
 
 func TestBlockLocalBlocked(t *testing.T) {
-	_, resp := doTestBlockLocal(t, []string{"localhost"}, "http://127.0.0.1/index.html")
+	_, resp := doTestBlockLocal(t, []string{"localhost"}, "http://127.0.0.1/index.html", &testResolver{127, 0, 0, 1})
 	assert.Equal(t, http.StatusForbidden, resp.StatusCode)
 }
 
 func TestBlockLocalPrivate(t *testing.T) {
-	_, resp := doTestBlockLocal(t, []string{"localhost"}, "http://192.168.0.1/index.html")
+	_, resp := doTestBlockLocal(t, []string{"localhost"}, "http://192.168.0.1/index.html", &testResolver{192, 168, 0, 1})
 	assert.Equal(t, http.StatusForbidden, resp.StatusCode)
 }
 
 func TestBlockLocalException(t *testing.T) {
-	_, resp := doTestBlockLocal(t, []string{"localhost"}, "http://localhost/index.html")
+	_, resp := doTestBlockLocal(t, []string{"localhost"}, "http://localhost/index.html", &testResolver{127, 0, 0, 1})
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 }
 
 func TestBlockLocalExceptionWithPort(t *testing.T) {
-	_, resp := doTestBlockLocal(t, []string{"127.0.0.1:7300"}, "http://127.0.0.1:7300/index.html")
+	_, resp := doTestBlockLocal(t, []string{"127.0.0.1:7300"}, "http://127.0.0.1:7300/index.html", &testResolver{127, 0, 0, 1})
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 }
 
 func TestBlockLocalNotLocal(t *testing.T) {
-	modifiedReq, resp := doTestBlockLocal(t, []string{"localhost"}, "http://example.com/index.html")
+	modifiedReq, resp := doTestBlockLocal(t, []string{"localhost"}, "http://example.com/index.html", &testResolver{93, 184, 215, 16})
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	// assert that block local filter did the resolving and modified req.URL.Host
 	exampleDotComIP := "93.184.215.16"
 	assert.Equal(t, exampleDotComIP, modifiedReq.URL.Host)
 }
 
-func doTestBlockLocal(t *testing.T, exceptions []string, urlStr string) (*http.Request, *http.Response) {
+func doTestBlockLocal(t *testing.T, exceptions []string, urlStr string, r resolver) (*http.Request, *http.Response) {
 	next := func(cs *filters.ConnectionState, req *http.Request) (*http.Response, *filters.ConnectionState, error) {
 		return &http.Response{
 			StatusCode: http.StatusOK,
 		}, cs, nil
 	}
 
-	filter := BlockLocal(exceptions, &testResolver{})
+	filter := BlockLocal(exceptions, r)
 	req, _ := http.NewRequest(http.MethodGet, urlStr, nil)
 	log.Debug(req.URL.Host)
 	cs := filters.NewConnectionState(req, nil, nil)
