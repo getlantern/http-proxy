8a52fd2274bf7de481c8034e2f720646f50283c5
WendelHime <6754291+WendelHime@users.noreply.github.com>
2024-06-26 13:22:54 -0300

    fix: keep using water instead of directly referring to v1

--- water/listener.go
+++ water/listener.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/getlantern/golog"
 	"github.com/refraction-networking/water"
-	v1 "github.com/refraction-networking/water/transport/v1"
+	_ "github.com/refraction-networking/water/transport/v1"
 )
 
 var log = golog.LoggerFor("water")
@@ -25,7 +25,7 @@ func NewWATERListener(ctx context.Context, baseListener net.Listener, wasm strin
 		ModuleConfigFactory: water.NewWazeroModuleConfigFactory(),
 	}
 
-	waterListener, err := v1.NewListenerWithContext(ctx, cfg)
+	waterListener, err := water.NewListenerWithContext(ctx, cfg)
 	if err != nil {
 		log.Errorf("error creating water listener: %v", err)
 		return nil, err
