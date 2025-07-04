53cb84198791ecac91c3dc0f93639c0a9c75c8f1
Allan Guwatudde <guwats10@gmail.com>
2025-07-04 19:19:06 +0300

    Add servermasq set up script comment

--- servermasq.sh
+++ servermasq.sh
@@ -8,6 +8,13 @@ if [ -z "$PROXY_ADDR" ] || [ -z "$PROXY_PORT" ] || [ -z "$MASQ_ADDR" ]; then
   exec "$@"
 fi
 
+# The iptables rules can be expalined as follows:
+# 1. Create a new chain called LANTERN_SERVERMASQ.
+# 2. Add a rule to the LANTERN_SERVERMASQ chain that matches packets destined for the proxy address
+#     (PROXY_ADDR) that are not destined for the proxy port (PROXY_PORT), and redirects them to the masqAddr.
+#  3. Add a rule to the PREROUTING chain that matches packets destined for the proxy address (PROXY_ADDR)
+#     and redirects them to the LANTERN_SERVERMASQ chain.
+
 iptables -t nat -N LANTERN_SERVERMASQ 2>/dev/null || true
 iptables -t nat -F LANTERN_SERVERMASQ 2>/dev/null || true
 
