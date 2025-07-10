c147d5e18a49f13fe2fab29974e22653e1069492
Allan Guwatudde <guwats10@gmail.com>
2025-07-10 13:10:10 +0300

    Fix iptables commands

--- servermasq.sh
+++ servermasq.sh
@@ -18,8 +18,8 @@ fi
 iptables -t nat -N LANTERN_SERVERMASQ 2>/dev/null || true
 iptables -t nat -F LANTERN_SERVERMASQ 2>/dev/null || true
 
-iptables -t nat -A LANTERN_SERVERMASQ -d "$PROXY_ADDR" ! --dport "$PROXY_PORT" -j DNAT --to-destination "$MASQ_ADDR"
+iptables -t nat -A LANTERN_SERVERMASQ -d "$PROXY_ADDR" -p tcp ! --dport "$PROXY_PORT" -j DNAT --to-destination "$MASQ_ADDR"
 iptables -t nat -A PREROUTING -d "$PROXY_ADDR" -j LANTERN_SERVERMASQ
 
 echo "[+] LANTERN_SERVERMASQ setup complete: $@"
-exec "$@"
+exec su-exec lantern "$@"
